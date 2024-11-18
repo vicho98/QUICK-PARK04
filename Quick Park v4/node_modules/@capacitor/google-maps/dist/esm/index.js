83f8f431630a48844d7c3492ebac8fb0b9c55591
chgodoyp <chgodoyp@gmail.com>
2024-11-18 16:54:00 -0300

    Revert "ordenar carpetas"
    This reverts commit e077e3d897f78461a173e452e8fcefe75f651227.

--- Quick Park v4/node_modules/@capacitor/google-maps/dist/esm/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-import { LatLngBounds, MapType } from './definitions';
-import { GoogleMap } from './map';
-export { GoogleMap, LatLngBounds, MapType };
-//# sourceMappingURL=index.js.map
