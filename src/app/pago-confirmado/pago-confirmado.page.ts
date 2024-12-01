936febc16856e2c9bcc52a9bbe2aca169d2e8ce8
BastianReyes25 <basty25xd@gmail.com>
2024-12-01 20:39:18 -0300

    agregue boton de volver despues de realizar pago

--- src/app/pago-confirmado/pago-confirmado.page.ts
+++ src/app/pago-confirmado/pago-confirmado.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';  // Importa NavController
 
 @Component({
   selector: 'app-pago-confirmado',
@@ -7,9 +8,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./pago-confirmado.page.scss'],
 })
 export class PagoConfirmadoPage {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private navCtrl: NavController  // Inyecta NavController
+  ) {}
 
   ngOnInit() {
     // Aquí puedes agregar la lógica para manejar la confirmación del pago
+  }
+
+  // Método para redirigir al home sin que el usuario pueda volver atrás
+  goHome() {
+    this.navCtrl.navigateRoot('/home');  // Redirige al home sin dejar rastro en el historial
   }
 }
