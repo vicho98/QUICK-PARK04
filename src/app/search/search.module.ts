2ff8a7fb595184c85cb14fbaa942296b219fb5a6
vicho98 <105834157+vicho98@users.noreply.github.com>
2024-12-03 23:11:46 -0300

    cagazo

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SearchPage } from './search.page';  // Asegúrate de que esta ruta sea correcta
+import { SearchPageRoutingModule } from './search-routing.module'; // Asegúrate de importar las rutas
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule
+    IonicModule,
+    SearchPageRoutingModule  // Asegúrate de incluir esto para que el enrutamiento funcione correctamente
   ],
   declarations: [SearchPage],
   exports: [SearchPage]  // Aquí exportas el componente para que se pueda usar en otros módulos
